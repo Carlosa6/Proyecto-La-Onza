7fb7c75ae18e01d59d5f776ef4cc985d5213071a
Carlosa6 <k5r6ri89yo@gmail.com>
2021-08-26 01:34:10 -0500

    Cambios en index e insercion de imagenes

---
+++ dist/main-parte1.js
@@ -0,0 +1,32 @@
+document.querySelector(".menu-btn").addEventListener("click", () => {
+    document.querySelector(".nav-menu").classList.toggle("show");
+  });
+  
+  //Leer json de imágenes
+  function cargarImg() {
+    const xhttp = new XMLHttpRequest();
+    xhttp.open("GET", "./json/imgl123.json", true);
+    xhttp.send();
+    xhttp.onreadystatechange = function () {
+      if (this.readyState == 4 && this.status == 200) {
+        let datos = JSON.parse(this.responseText);
+        // console.log(datos);
+        const compania = document.getElementById('compania')
+        compania.innerHTML = ''
+  
+        datos.map(function(imagen){
+          compania.innerHTML += `
+            <div id="${imagen.id}" class="foto-onza">
+              <img src="${imagen.img}" alt="${imagen.id}" />
+              <div class="descripcion">
+                <p>${imagen.desc}</p>
+              </div>
+            </div>
+          `
+        })
+      }
+    };
+  }
+  
+  window.onload = cargarImg;
+  
