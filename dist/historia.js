cd35430cce1f9fc19f6574eb4f9719d7cbb53fbf
Carlosa6 <k5r6ri89yo@gmail.com>
2021-09-17 01:38:19 -0500

    terminado agregacion de imagenes en historia

--- dist/historia.js
+++ dist/historia.js
@@ -21,29 +21,3 @@
     };
   }
 
-  function cargarImgGeologia() {
-    const xhttp = new XMLHttpRequest();
-    xhttp.open("GET", "./json/historia-geologia.json", true);
-    xhttp.send();
-    xhttp.onreadystatechange = function () {
-      if (this.readyState == 4 && this.status == 200) {
-        let datos = JSON.parse(this.responseText);
-        // console.log(datos);
-        const compania = document.getElementById('slide-geologia')
-        compania.innerHTML = ''
-  
-        datos.map(function(imagen){
-          compania.innerHTML += `
-            <div id="${imagen.id}" class="swiper-slide">
-              <img src="${imagen.img}" alt="${imagen.id}" />
-            </div>
-          `
-        })
-      }
-    };
-  }
-
-
-
-
-
